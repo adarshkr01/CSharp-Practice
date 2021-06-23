fa083ada7557c8d820ad81731d0fec8a6a6d782d
Niekon01 <adarsh90909090@gmail.com>
2021-06-23 11:48:29 +0530

    Practice of delegates and Events

---
+++ Delegates and Events/UserSignup/UserSignup/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace UserSignup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User() { Username = "admin", Age = 21 };
+
+            Authentication auth = new Authentication();
+
+            // functions to be run by delegate
+            Action<User> authenticationList = auth.CheckUsername;
+            authenticationList += auth.CheckPassword;
+            authenticationList += auth.CheckUniqueUsername;
+
+            UserSignup userSignup = new UserSignup();
+
+            // Event
+            SendEmail sendEmail = new SendEmail();
+            userSignup.UserSignedUp += sendEmail.SendConfirmationEmail;
+
+            userSignup.AuthenticateUser(user, authenticationList);
+        }
+    }
+}
