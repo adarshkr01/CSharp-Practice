f61cae11cbdb0e9f24163cfdbf25428c06dbfca3
Niekon01 <adarsh90909090@gmail.com>
2021-06-24 11:28:59 +0530

    Linq basics

---
+++ LinqPractice/LinqPractice/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<Book> books = new BookRepository().GetBooks();
+
+            //// Linq Query Operators
+            //var cheaperBooks =
+            //                    from b in books
+            //                    where b.Price < 15
+            //                    orderby b.Title
+            //                    select b;
+
+            // Linq Extension Methods
+            var cheapBooks = books
+                                .Where(b => b.Price < 15)
+                                .OrderBy(b => b.Title);
+
+            foreach(Book book in cheapBooks)
+            {
+                Console.WriteLine(book.Title);
+            }
+
+            var cheapBooksTitle = cheapBooks.Select(b => b.Title);
+        }
+    }
+}
