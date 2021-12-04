48fbf8bbafb682003e0d585d0132e11d6ba1e7ad
Niekon01 <adarsh90909090@gmail.com>
2021-12-04 09:09:08 +0530

    new

---
+++ PracticeQuestions/PracticeQuestions/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PracticeQuestions
+{
+    public class Generic<T>
+    {
+        Stack<T> stk = new Stack<T>();
+        public void push(T obj)
+        {
+            stk.Push(obj);
+        }
+        public T pop()
+        {
+            T obj = stk.Pop();
+            return obj;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Generic<int> g = new Generic<int>();
+            g.push(50);
+            Console.WriteLine(g.pop());
+            Console.ReadLine();
+        }
+    }
+}
